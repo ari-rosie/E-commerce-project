148497c441a65df560cb3925f7f4a6378f66d040
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-10-19 16:15:46 -0400

    build

---
+++ client/build/precache-manifest.a5b5dc9fbf1914ba3dcd4706162bb1a1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae6cc3c3b46c80e23540e01fd390017",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c94b3e9f167ebd722077",
+    "url": "/static/css/2.653bbd99.chunk.css"
+  },
+  {
+    "revision": "c94b3e9f167ebd722077",
+    "url": "/static/js/2.e3c0b4dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e3c0b4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0878d9ec36b068dc2408",
+    "url": "/static/js/main.4251bf34.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "96d1dfbf39ee267cc7c371f8c6cd7500",
+    "url": "/static/media/hero-banner.96d1dfbf.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
